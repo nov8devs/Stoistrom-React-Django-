30c6d9d3e11c6e35ab2964997fc8a9cab91bbf45
huzaifah-here <huzaifahkhan00@gmail.com>
2023-11-21 23:47:52 +0500

    First Commit by huzaifa

---
+++ README.md
@@ -0,0 +1,20 @@
+To run this project, first do:
+
+    git clone https://github.com/nov8devs/Stoistrom-React-Django-.git
+
+After cloning you have 3 files which is Frontend, Backend and venv.
+You are in this directory "Stoistrom-React-Django-" Open Vscode or any workspace you like. You have 2 terminal open.
+
+# Frontend - React
+
+    cd frontend
+    npm i
+    npm run dev
+
+You will have page for react
+
+# Backend - Django
+
+    cd backend
+    pip install django-cors-headers pillow
+    python manage.py runserver
--- frontend/src/components/JournalingChoices.jsx
+++ frontend/src/components/JournalingChoices.jsx
@@ -1,19 +1,21 @@
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+
+// import { useHistory } from 'react-router-dom'
 
 function JournalingChoices() {
-    const history = useHistory()
+  // const history = useHistory()
+  const nav = useNavigate();
+  const navigateToEmptyPage = () => {
+    // history.push('/journal')
+    nav("/journal");
+  };
 
-    const navigateToEmptyPage = () =>{
-        history.push('/journal')
-    }
-
-    return (
-        <div>
-            <button onClick={navigateToEmptyPage}>Empty Page</button>
-            <button>Random Prompt</button>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={navigateToEmptyPage}>Empty Page</button>
+      <button>Random Prompt</button>
+    </div>
+  );
 }
-
 
 export default JournalingChoices;
